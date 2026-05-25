9adf1c54a53e60e4795c6c13b190304bf691af7b
Argel dos Santos <argeldossantos@outlook.com>
2026-05-25 03:41:17 -0300

    Model de ReceitaMedicamento

--- prontuarioapp/apps/receitamedicamento/models.py
+++ prontuarioapp/apps/receitamedicamento/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class ReceitaMedicamento(models.Model):
+    frequencia = models.CharField(max_length=100, verbose_name="Frequência")
+    duracao_dias = models.IntegerField(verbose_name="Duração em dias")
+    dose = models.CharField(max_length=100, verbose_name="Dose")
+    concentracao = models.CharField(max_length=100, verbose_name="Concentração")
+    
+    
+    receita = models.ForeignKey('receita.Receita', on_delete=models.CASCADE, related_name='itens_receita')
+    medicamento = models.ForeignKey('medicamento.Medicamento', on_delete=models.CASCADE, related_name='itens_receita')
+
+    def __str__(self):
+        return f"{self.medicamento} - {self.dose} (Receita: {self.receita.id})"
