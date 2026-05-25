d937b316658ab53c6106492d2a50b5d0667a3bb5
Jonathan <toledojts@outlook.com>
2026-05-25 02:10:27 -0300

    backup de seguranca

--- prontuarioapp/prontuarioapp/settings.py
+++ prontuarioapp/prontuarioapp/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'pessoa.apps.PessoaConfig',
+    'medico.apps.MedicoConfig',
+    'paciente.apps.PacienteConfig',
+    'anamnese.apps.AnamneseConfig',
 ]
 
 MIDDLEWARE = [
