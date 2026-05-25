58f6ccd40d47ce900991a7bc9fc981a907d0ce2e
Argel dos Santos <argeldossantos@outlook.com>
2026-05-25 15:50:19 -0300

    Urls atualizadas

--- prontuarioapp/apps/medicamento/urls.py
+++ prontuarioapp/apps/medicamento/urls.py
@@ -8,5 +8,9 @@ router = routers.SimpleRouter()
 router.register('', views.MedicamentoViewSet, basename='medicamento')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api', include(router.urls)),
+    path('list/', views.list_medico, name='list_medico'),
+    path('add/', views.add_medico, name='add_medico'),
+    path('edit/<int:id_medico>/', views.edit_medico, name='edit_medico'),
+    path('delete/<int:id_medico>/', views.delete_medico, name='delete_medico'),
 ]
