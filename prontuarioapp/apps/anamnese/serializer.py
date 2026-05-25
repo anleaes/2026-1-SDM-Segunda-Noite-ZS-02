55400e5ef9d54df618ea41a2622d0e8bb414b8d1
Jonathan <toledojts@outlook.com>
2026-05-25 01:10:20 -0300

    serializer implantado

--- prontuarioapp/apps/anamnese/serializer.py
+++ prontuarioapp/apps/anamnese/serializer.py
@@ -1,1 +1,7 @@
-iniciando anamnese
+from .models import Anamnese
+from rest_framework import serializers
+
+class AnamneseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Anamnese
+        fields = '__all__'
