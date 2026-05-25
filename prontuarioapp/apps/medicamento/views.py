43a247b04829ba9960da01959ca30cbcb362cd3b
Argel dos Santos <argeldossantos@outlook.com>
2026-05-25 01:15:16 -0300

    Atulizei o serializer

--- prontuarioapp/apps/medicamento/views.py
+++ prontuarioapp/apps/medicamento/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Medicamento
-from .serializers import MedicamentoSerializer
+from .serializer import MedicamentoSerializer
 
 class MedicamentoViewSet(viewsets.ModelViewSet):
     queryset = Medicamento.objects.all()
