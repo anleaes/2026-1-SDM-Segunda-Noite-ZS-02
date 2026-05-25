83c5f84916c579314be1ca96f252494f5e03e427
Jonathan <toledojts@outlook.com>
2026-05-25 01:08:34 -0300

    serializer medico ok

--- prontuarioapp/apps/medico/serializer.py
+++ prontuarioapp/apps/medico/serializer.py
@@ -1,1 +1,7 @@
-iniciando
+from .models import Medico
+from rest_framework import serializers
+
+class MedicoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Medico
+        fields = '__all__'
