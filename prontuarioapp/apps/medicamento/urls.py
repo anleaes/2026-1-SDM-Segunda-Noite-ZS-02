56ae9cd9dfc781daa18571a4c47b683c96420993
Argel dos Santos <argeldossantos@outlook.com>
2026-05-25 00:57:56 -0300

    Configuração das rotas de medicamento

--- prontuarioapp/apps/medicamento/urls.py
+++ prontuarioapp/apps/medicamento/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import MedicamentoViewSet
+from . import views
+from rest_framework import routers
+
+app_name = 'medicamento'
+
+router = routers.SimpleRouter()
+router.register('', views.MedicamentoViewSet, basename='medicamento')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
