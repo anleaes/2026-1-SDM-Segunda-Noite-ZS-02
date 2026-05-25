5a4d1c945a95cfe7967ea9179488ff0c9968c89d
Argel dos Santos <argeldossantos@outlook.com>
2026-05-24 23:58:17 -0300

    criacao do arquivo de views

--- prontuarioapp/apps/medicamento/views.py
+++ prontuarioapp/apps/medicamento/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Medicamento
+from .serializers import MedicamentoSerializer
 
 # Create your views here.
