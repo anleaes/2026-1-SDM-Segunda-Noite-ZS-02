f0d286642e6172b3ad464228499980f134ee364b
Argel dos Santos <argeldossantos@outlook.com>
2026-05-25 03:46:43 -0300

    Imports para views de receitamedicamento

--- prontuarioapp/apps/receitamedicamento/views.py
+++ prontuarioapp/apps/receitamedicamento/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ReceitaMedicamento
+from .serializer import ReceitaMedicamentoSerializer
+
 
 # Create your views here.
