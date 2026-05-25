17f99ad7589970e4adeb8eeb69f780eb67e3bab8
Jonathan <toledojts@outlook.com>
2026-05-25 01:05:39 -0300

    serializer paciente implantado

--- prontuarioapp/apps/paciente/serializer.py
+++ prontuarioapp/apps/paciente/serializer.py
@@ -1,1 +1,7 @@
-iniciando
+from .models import Paciente
+from rest_framework import serializers
+
+class PacienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paciente
+        fields = '__all__'
