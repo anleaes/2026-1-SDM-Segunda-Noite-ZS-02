78e23a49e3d21aa17cfad093e1b94e87c1b46589
arthurw26 <arthur.wiatrowski@gmail.com>
2026-05-24 23:15:06 -0300

    Criacao add_cid, criacao forms.py, criacao add_cid.html

--- prontuarioapp/apps/cid/views.py
+++ prontuarioapp/apps/cid/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Employee
+from .models import Cid
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import viewsets
+from .forms import CidForm
 from .serializer import CidSerializer
 
 # Create your views here.
@@ -8,3 +10,20 @@ from .serializer import CidSerializer
 class CidViewSet(viewsets.ModelViewSet):
     queryset = Cid.objects.all()
     serializer_class = CidSerializer  
+
+
+def add_cid(request):
+    template_name = 'cid/add_cid.html'
+    context = {}
+
+    if request.method == 'POST':
+        form = CidForm(request.POST)
+
+        if form.is_valid():
+            cid = form.save(commit=False)
+            cid.save()
+            return redirect('cid:list_cid')
+
+    form = CidForm()
+    context['form'] = form
+    return render(request, template_name, context)
