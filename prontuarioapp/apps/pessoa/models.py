62ded9b797007449cd15d8ba8a6a6c8afe521ae9
Jonathan <toledojts@outlook.com>
2026-05-25 02:07:39 -0300

    up da minha parte

---
+++ prontuarioapp/apps/pessoa/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here. mudancas abaixo apenas para fechar os dois commits
+
+
+class Pessoa(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    sobrenome = models.CharField('Sobrenome', max_length=100) 
+    cpf = models.CharField('CPF', max_length=11, unique=True)   
+    telefone = models.CharField('Telefone celular', max_length=20)
+    email = models.EmailField('E-mail', null=False, blank=False, unique=True)
+
+    class Meta:
+        verbose_name = 'Pessoa'
+        verbose_name_plural = 'Pessoas'
+        ordering =['id']
+
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
