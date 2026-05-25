b22b1fce84e69422c667dba53df58062f6198db1
arthurw26 <arthur.wiatrowski@gmail.com>
2026-05-24 22:30:53 -0300

    Criacao models.py cid

--- prontuarioapp/apps/cid/models.py
+++ prontuarioapp/apps/cid/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cid(models.Model):
+    cod_cid = models.CharField(max_length=10, unique=True)
+    descricao = models.CharField(max_length=255)
+    sintomas = models.TextField()
+    tipo = models.CharField(max_length=100)
+    status_ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.cod_cid} - {self.descricao}"
