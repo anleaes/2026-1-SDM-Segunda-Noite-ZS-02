d466e26a86b0c49dbd8f629c249635b93366ec42
Argel dos Santos <argeldossantos@outlook.com>
2026-05-25 03:01:14 -0300

    implementando ConsultaViewSet

--- prontuarioapp/apps/consulta/views.py
+++ prontuarioapp/apps/consulta/views.py
@@ -3,3 +3,6 @@ from .models import Consulta
 from .serializer import ConsultaSerializer
 
 # Create your views here.
+class ConsultaViewSet(viewsets.ModelViewSet):
+    queryset = Consulta.objects.all()
+    serializer_class = ConsultaSerializer
