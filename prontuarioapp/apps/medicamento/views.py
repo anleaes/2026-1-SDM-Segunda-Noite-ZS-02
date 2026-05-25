8acbf09215901ac6dea26aced6d82c3df75df8cf
Argel dos Santos <argeldossantos@outlook.com>
2026-05-25 00:20:04 -0300

    Implementacao do MedicamentoViewSet

--- prontuarioapp/apps/medicamento/views.py
+++ prontuarioapp/apps/medicamento/views.py
@@ -3,4 +3,6 @@ from rest_framework import viewsets
 from .models import Medicamento
 from .serializers import MedicamentoSerializer
 
-# Create your views here.
+class MedicamentoViewSet(viewsets.ModelViewSet):
+    queryset = Medicamento.objects.all()
+    serializer_class = MedicamentoSerializer
