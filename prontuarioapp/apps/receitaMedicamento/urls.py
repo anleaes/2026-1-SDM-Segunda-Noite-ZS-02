34c00c462e137bc04519a859593cfa0134380680
Argel dos Santos <argeldossantos@outlook.com>
2026-05-26 22:35:46 -0300

    Registrando rota api

--- prontuarioapp/apps/receitaMedicamento/urls.py
+++ prontuarioapp/apps/receitaMedicamento/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 app_name = 'receitamedicamento'
 
+router = routers.SimpleRouter()
+#Registra APENAS a rota da API (/receitaMedicamento/api/)
+router.register('api', views.ReceitaMedicamentoViewSet, basename='receitamedicamento-api')
+
 urlpatterns = [
    
 ]
