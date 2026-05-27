9fa615ddb0a08be17585cad0db2db0c1ca4e20e8
Argel dos Santos <argeldossantos@outlook.com>
2026-05-26 22:43:27 -0300

    função das telas

--- prontuarioapp/apps/receitaMedicamento/views.py
+++ prontuarioapp/apps/receitaMedicamento/views.py
@@ -8,3 +8,15 @@ from .models import ReceitaMedicamento
 class ReceitaMedicamentoViewSet(viewsets.ModelViewSet):
     queryset = ReceitaMedicamento.objects.all()
     serializer_class = ReceitaMedicamentoSerializer
+
+#FUNÇÕES DAS TELAS COMEÇAM AQUI
+
+def list_receitamedicamento(request):
+    # Busca todas as ligações de receita e medicamento no banco
+    receitas_med = ReceitaMedicamento.objects.all()
+    
+    context = {
+        'receitas_med': receitas_med
+    }
+    # Manda os dados para a tela HTML
+    return render(request, 'receitaMedicamento/list.html', context)
