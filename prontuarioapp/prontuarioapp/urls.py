e4e79e219934bfccc6ea0913efabca411d665f51
Argel dos Santos <argeldossantos@outlook.com>
2026-05-26 21:17:20 -0300

    registo rota de receitaMedicamento no urls.py principal

--- prontuarioapp/prontuarioapp/urls.py
+++ prontuarioapp/prontuarioapp/urls.py
@@ -31,5 +31,7 @@ urlpatterns = [
     path('consulta/', include('consulta.urls')),
     path('medicamento/', include('medicamento.urls', namespace='medicamento')),
     path('receita/', include('receita.urls', namespace='receita')),
+    path('receitaMedicamento/', include('receitaMedicamento.urls', namespace='receitamedicamento')),
+
 ]
 
