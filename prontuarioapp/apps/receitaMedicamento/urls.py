266a8accebd38592dcd8b6f1b70079879948b324
Argel dos Santos <argeldossantos@outlook.com>
2026-05-26 22:50:26 -0300

    fix: adicionei o  import do routers ausente no urls.py

--- prontuarioapp/apps/receitaMedicamento/urls.py
+++ prontuarioapp/apps/receitaMedicamento/urls.py
@@ -1,12 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from . import views
+from rest_framework import routers  # <-- É essa linha aqui que estava faltando!
 
 app_name = 'receitamedicamento'
 
 router = routers.SimpleRouter()
-#Registra APENAS a rota da API (/receitaMedicamento/api/)
 router.register('api', views.ReceitaMedicamentoViewSet, basename='receitamedicamento-api')
 
 urlpatterns = [
+   
     path('', include(router.urls)),
+    
+   
     path('list/', views.list_receitamedicamento, name='list_receitamedicamento'),
 ]
