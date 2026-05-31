def7f11a7debe899a1c0eea162b95d32a4e0339b
Argel dos Santos <argeldossantos@outlook.com>
2026-05-31 19:44:18 -0300

    adiciona django-cors-headers no settings para liberar acesso ao vue

--- prontuarioapp/prontuarioapp/settings.py
+++ prontuarioapp/prontuarioapp/settings.py
@@ -56,9 +56,11 @@ INSTALLED_APPS = [
     'medicamento.apps.MedicamentoConfig',
     'receita.apps.ReceitaConfig',
     'receitaMedicamento.apps.ReceitamedicamentoConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -144,4 +146,9 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
 ]
